312fdf585d5842fda6df6cc6cce8cab3caa3008c
yanglaoshi <yanglaoshi.com>
2023-12-25 22:26:13 +0800

    change 1.c on masetr

--- 1.c
+++ 1.c
@@ -5,6 +5,15 @@ int main()
 	printf("nihao yanglaoshi \n");
 	printf("dev1 \n");
 	printf("git branch\n");
+
+	//下面代码内容，为在master主线分支上对代码的修改
+	int b;
+	for(b = 0; b < 5; b++)
+	{
+		printf("%d -",b);
+		putchar(10);
+	}
+
 	return 0;
 	
 }

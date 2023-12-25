c6818434e793da8362effeed31f5d5e3bd153681
yanglaoshi <yanglaoshi.com>
2023-12-25 22:30:42 +0800

    change 1.c on dev1

--- 1.c
+++ 1.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 int main()
-{       int a;
-	printf("nihao yanglaoshi \n");
-	printf("dev1 \n");
-	printf("git branch\n");
+{       
+	//以下代码，在dev1 分支上进行修改
+	int a;
+	for(a = 0; a < 5; a++)
+	{
+	
+		printf("nihao yanglaoshi \n");
+		printf("dev1 \n");
+		printf("git branch\n");
+	}
+
 	return 0;
 	
 }
